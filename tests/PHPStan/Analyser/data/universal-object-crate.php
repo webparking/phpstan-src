ea17a042c74fd71c8c298e9014a765eecb140429
Jeroen van Oort <jvanoort@simplexis.nl>
2020-01-09 21:51:36 +0100

    Added test case

--- tests/PHPStan/Analyser/data/universal-object-crate.php
+++ tests/PHPStan/Analyser/data/universal-object-crate.php
@@ -2,6 +2,9 @@
 
 namespace UniversalObjectCrate;
 
+/**
+ * @property Baz $baz
+ */
 class Foo extends \stdClass
 {
 
@@ -17,6 +20,7 @@ class Foo extends \stdClass
 	{
 		$this->doBar($this->name);
 		$this->doBaz($this->name); // reported - string passed to int
+		$this->doQur($this->baz);
 	}
 
 	public function doBar(string $name): void
@@ -28,6 +32,29 @@ class Foo extends \stdClass
 	{
 
 	}
+
+	public function doQur(Baz $baz): void
+	{
+
+	}
+
+	public function __get(string $name): Bar
+	{
+		if ('baz' === $name) {
+			return new Baz();
+		}
+
+		return new Bar();
+	}
+}
+
+class Bar extends \stdClass
+{
+
+}
+
+class Baz extends Bar
+{
 
 }
 
